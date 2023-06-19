c897ee3705272dd09c864dc3d7598935b4053eb0
Samir Rodas <serodas9710@gmail.com>
2023-06-18 19:42:38 -0500

    Add User crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Post;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -33,5 +34,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Categorias', 'fas fa-folder', Category::class);
         yield MenuItem::linkToCrud('Publicaciones', 'fas fa-cloud', Post::class);
         yield MenuItem::linkToCrud('Comentarios', 'fas fa-comment', Comment::class);
+        yield MenuItem::linkToCrud('Usuarios', 'fas fa-users', User::class);
     }
 }
