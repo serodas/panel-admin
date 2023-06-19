b81b91bba4d78db28c29ef47d2e545a974878190
Samir Rodas <serodas9710@gmail.com>
2023-06-18 22:01:00 -0500

    Add page blog

---
+++ src/Controller/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(PostRepository $postRepository): Response
+    {
+        return $this->render('page/home.html.twig', [
+            'posts' => $postRepository->findLatest()
+        ]);
+    }
+
+    #[Route('/blog/{slug}', 'app_post')]
+    public function post(Post $post): Response
+    {
+        return $this->render('page/post.html.twig', [
+            'post' => $post
+        ]);
+    }
+}
